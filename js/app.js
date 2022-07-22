03fa22e2be8e38f0cabce5ca51fa6a82e6c9447e
1_b1n <choi44601280@gmail.com>
2022-07-22 16:12:12 +0900

    헤더성공

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
+// // 스크롤 감지해서 헤더 숨기기
+// $(window).bind('mousewheel', function(event) {
+//     if (event.originalEvent.wheelDelta >= 0) {
+//         console.log('Scroll up');
+//         $('#header').slideDown(500);
+//         $('#header .logo').slideDown(500);        
+//     }
+//     else {
+//         console.log('Scroll down');
+//         $('#header').slideUp(500);
+//         $('#header .logo').slideUp(500);
+//     }
+// });
+
+
+
+
 // 스크롤 감지해서 헤더 숨기기
 $(window).bind('mousewheel', function(event) {
     if (event.originalEvent.wheelDelta >= 0) {
         console.log('Scroll up');
-        $('#header').slideDown(500);
+        $('#header').css('transition-duration', '0.5s').css('transform', 'translateY(0)');
+        $('#header .logo').css('transition-duration', '0.5s').css('transform', 'translateY(0)');
     }
     else {
         console.log('Scroll down');
-        $('#header').slideUp(500);
+        $('#header').css('transition-duration', '0.5s').css('transform', 'translateY(-100%)');
+        $('#header .logo').css('transition-duration', '0.5s').css('transform', 'translateY(-100%)');
     }
 });
